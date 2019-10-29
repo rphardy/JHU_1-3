9e9e775141373c085c711215b55d992b71cf1adb
Richard Hardy <rphardy497@gmail.com>
2019-10-30 07:11:11 +1100

    Cleaned up pollutantmean.R code

--- RProgrammingCourse/Week2/Week2_Assignment/pollutantmean.R
+++ RProgrammingCourse/Week2/Week2_Assignment/pollutantmean.R
@@ -6,31 +6,6 @@ dataseturl <- "https://d396qusza40orc.cloudfront.net/rprog%2Fdata%2Fspecdata.zip
 download.file(dataseturl, destfile="rprog_data_specdata.zip")
 unzip("rprog_data_specdata.zip")
 
-list.files("specdata")
-file001 <- read.csv("specdata/001.csv")
-
-files <- list.files("specdata")
-files_full <- list.files("specdata", full.names = TRUE)
-
-head(read.csv(files_full[1]))
-head(read.csv(files_full[2]))
-head(read.csv(files_full[3]))
-
-file001_file002 <- rbind(file001, read.csv(files_full[2]))
-
-pollutant <- function(directory, id = 1:332){
-  files_list <- list.files(directory, full.names=TRUE)
-  dat <- data.frame()
-  for (i in id){
-    dat <- rbind(dat, read.csv(files_full[i]))
-  }
-  str(dat)
-}
-
-#test so far
-pollutant("specdata", 1:3)
-
-#now for pollutantmean
 pollutantmean <- function(directory, pollutant, id = 1:332){
   files_list <- list.files(directory, full.names=TRUE)
   dat <- data.frame()
